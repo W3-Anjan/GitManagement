8e3f5dff53a997ec91386ba7515af3a9861bff66
Anjan Debnath <anjan@w3engineers.com>
2019-08-21 16:16:30 +0600

    Tag added

--- app/src/main/java/com/pm/gitmanagement/MainActivity.java
+++ app/src/main/java/com/pm/gitmanagement/MainActivity.java
@@ -2,6 +2,7 @@ package com.pm.gitmanagement;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(getApplicationContext(), "test", Toast.LENGTH_LONG).show();
+        Log.d("tag","This is a test message");
     }
 }
